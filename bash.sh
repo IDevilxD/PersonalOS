4d13cf7b28df43aa1813f6576e9ac0ab746f5150
server <98081526+IDevilxD@users.noreply.github.com>
2022-04-18 20:43:09 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,8 @@ echo "H. E. L. L. O"
 
 sleep 1
 
-echo " S. T. A. R. T. I. N. G.   M. A. I. N.   F. I. L. E."
+echo ""
+
+echo "S. T. A. R. T. I. N. G.   M. A. I. N.   F. I. L. E."
 
 python mainBash.py
