c43e5ccd8f6d3051e7892e074a1ccfc80060ccb2
server <98081526+IDevilxD@users.noreply.github.com>
2022-04-19 14:06:15 +0530

    Update function.py

--- RESOURCES/function.py
+++ RESOURCES/function.py
@@ -3,4 +3,4 @@ import os
 def update():
   os.system("rm -rf PersonalOS")
   os.system("git clone https://github.com/IDevilxD/PersonalOS")
-  os.system("python PersonalOS")
+  os.system("python Personal")
